05fec496527c465dd51612e5a7ecc88f467874ee
not-soo-cool <yashvendrasengar1111@gmail.com>
2023-12-02 23:31:26 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import About from "./Components/About/About";
+import Projects from "./Components/Projects/Projects";
+import Contact from "./Components/Contact/Contact";
+import Login from "./Components/Login/Login";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser, loadUser } from "./actions/user";
+import AdminPanel from "./Components/Admin/AdminPanel";
+import Timeline from "./Components/Admin/Timeline";
+import Youtube from "./Components/Admin/Youtube";
+import Project from "./Components/Admin/Project";
+import Loader from "./Components/Loader/Loader";
+
+function App() {
+  const dispatch = useDispatch();
+
+  const { isAuthenticated } = useSelector((state) => state.login);
+  const { loading, user } = useSelector((state) => state.user);
+
+  console.log(user);
+
+  useEffect(() => {
+    dispatch(getUser());
+    dispatch(loadUser());
+  }, [dispatch]);
+
+  return (
+    <Router>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Home
+                  youtubes={user && user.youtube}
+                  timelines={user && user.timeline}
+                  skills={user && user.skills}
+                  
+                />
+              }
+            />
+            <Route path="/about" element={<About 
+            about={user && user.about} 
+            />} />
+            <Route
+              path="/projects"
+              element={<Projects 
+                projects={user && user.projects} 
+                />}
+            />
+            <Route path="/contact" element={<Contact />} />
+            <Route
+              path="/account"
+              element={isAuthenticated ? 
+              <AdminPanel /> : <Login />}
+            />
+            <Route
+              path="/admin/timeline"
+              element={isAuthenticated ? <Timeline /> : <Login />}
+            />
+            <Route
+              path="/admin/youtube"
+              element={isAuthenticated ? <Youtube /> : <Login />}
+            />
+
+            <Route
+              path="/admin/project"
+              element={isAuthenticated ? <Project /> : <Login />}
+            />
+          </Routes>
+
+          <Footer />
+        </>
+      )}
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { Provider as AlertProvider, positions, transitions } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+import store from "./store";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if(process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
+
+const options = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  transition: transitions.SCALE,
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <AlertProvider template={AlertTemplate} {...options}>
+        <App />
+      </AlertProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
